9b55581b92608dbab4ac333d80ced130ef6917bd
Philipp Heinrich <philipp.heinrich@fau.de>
2025-03-05 14:23:05 +0100

    sort and tidy up tests

--- tests/extensions/test_annoy.py
+++
@@ -1,7 +0,0 @@
-from semmap.semspace import SemMap
-
-
-def test_embeddings(brexit_corpus):
-    semspace = SemMap()
-    embeddings = semspace._embeddings(['angela', 'merkel'])
-    print(embeddings)
